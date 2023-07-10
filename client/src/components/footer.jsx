393633619f9fc9beb579eeb2731c92dda0cd8787
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-10 21:59:16 +0900

    직업은 개발자!

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from'react'
+
+function FooterCompoenet(props){
+  return(
+    <div>
+      <h2>직업은 개발자 !</h2>
+    </div>
+  )
+}
+
+export default FooterCompoenet;
---
+++ client/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderComponent(props){
+  return (
+    <div>
+      <h1>이경택입니다</h1>
+    </div>
+  )
+}
+
+export default HeaderComponent;
