ba7b0d9dda4c736c9505a06393af213ae7fd45a6
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-12 12:02:29 +0900

    구조는완성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import AboutMe from './components/aboutMe'
 import Project from './components/project'
+import Link from './components/Link'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
+      <div className='AppHeader'>
+        Port Folio
+      </div>
       <AboutMe />
       <Project />
+      <Link />
     </div>
   );
 }
---
+++ client/src/components/Link.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../css/components.css';
+
+function LinkComponent(props) {
+  return (
+    <div className='link'>
+      <div className='linkHeader'>
+        link1
+      </div>
+      <div className='linkFooter'>
+        link2
+      </div>
+    </div>
+  )
+}
+
+export default LinkComponent;
