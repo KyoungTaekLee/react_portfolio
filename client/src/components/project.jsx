9512c97b673ae6b95fb1250395c6102276658a69
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-12 18:02:48 +0900

    project 내용 추가 중

--- client/src/components/project.jsx
+++ client/src/components/project.jsx
@@ -8,8 +8,28 @@ function ProjectComponent(props) {
         Project
       </div>
       <div className='projectFooter'>
-        <div className="projectFooterImgOne">
-          <img src={require("./../img/projectC.png")} alt="" className="projectFooterImgOneSize" />
+        <div className="projectFooterImg">
+          <img src={require("./../img/projectC.png")} className="projectFooterImgSize" />
+          <div className="projectFooterImgContent">
+            ● 프로젝트 명 : 요너두<br></br>
+            ● 주요 역할 : 바닐라 JS Layout, Mysql 테이블 구성, CRUD기능, 서버 라우터 작업<br></br>
+            ● 사용 기술 : JavaScript, Node.JS, Mysql<br></br>
+            ● GitHub : https://github.com/KyoungTaekLee/KDT-2-Project-C-1
+          </div>
+        </div>
+        <div className="projectFooterImg">
+          <img src={require("./../img/projectA.png")} className="projectFooterImgSize" />
+          <div className="projectFooterImgContent">
+            하이
+            하이이
+          </div>
+        </div>
+        <div className="projectFooterImg">
+          <img src={require("./../img/projectB.png")} className="projectFooterImgSize" />
+          <div className="projectFooterImgContent">
+            하이
+            하이이
+          </div>
         </div>
       </div>
     </div>
