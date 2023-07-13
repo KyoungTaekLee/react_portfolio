fd955e8a042e9c8c0b632205519c294d3de5c2af
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-13 22:48:50 +0900

    노션업그레이드

--- client/src/components/Link.jsx
+++ client/src/components/Link.jsx
@@ -8,13 +8,17 @@ function LinkComponent(props) {
         Link
       </div>
       <div className='linkFooter'>
-        <a href="https://github.com/KyoungTaekLee" className='no-underline'>
-        <img src={require("./../img/GitHub.png")} className="imgSize" />
-        </a>
-        
-        <a href="https://blog.naver.com/developer_taek" className='no-underline'>
-        <img src={require("./../img/Naver.png")} className="imgSize" />
-        </a>
+        <div className='linkFooterSet'>
+          <a href="https://github.com/KyoungTaekLee" className='no-underline'>
+            <img src={require("./../img/GitHub.png")} className="imgSize" />
+          </a>
+          <a href="https://blog.naver.com/developer_taek" className='no-underline'>
+            <img src={require("./../img/Naver.png")} className="imgSize" />
+          </a>
+          <a href="https://www.notion.so/KDT_-af08b1222156412394307889ebee57fc" className='no-underline'>
+            <img src={require("./../img/notion.png")} className="imgSize" />
+          </a>
+        </div>
       </div>
     </div>
   )
