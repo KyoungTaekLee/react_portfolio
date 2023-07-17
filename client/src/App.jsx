38a7f2a1e4c9e1bad57a49ed2773d2e240cace0c
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-17 12:21:26 +0900

    섹션별로 이동 완료

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import Skill from './components/skill'
 import Project from './components/project'
 import Link from './components/Link'
 import './App.css'
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, Component } from 'react';
+import { Events, animateScroll as scroll } from 'react-scroll';
 
 function App() {
   const targetTop = useRef(null);
@@ -12,7 +13,26 @@ function App() {
   const targetProject = useRef(null);
   const targetLink = useRef(null);
 
-  const [scrollEffectEnabled, setScrollEffectEnabled] = useState(false);
+  const handleScroll = (event) => {
+    // 마우스 휠을 아래로 스크롤할 때마다 페이지가 100vh씩 이동하도록 처리
+    if (event.deltaY > 0) {
+      const windowHeight = window.innerHeight;
+      scroll.scrollMore(windowHeight);
+    } else if (event.deltaY < 0) {
+      const windowHeight = window.innerHeight;
+      scroll.scrollMore(-windowHeight);
+    }
+  };
+
+  useEffect(() => {
+    // 스크롤 이벤트 등록
+    window.addEventListener('wheel', handleScroll);
+
+    // 컴포넌트가 언마운트될 때 스크롤 이벤트 제거
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
 
   const TopClick = () => {
     targetTop.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -34,43 +54,11 @@ function App() {
     targetLink.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
-
-  useEffect(() => {
-    setScrollEffectEnabled(false);
-
-    const handleScroll = () => {
-      if (scrollEffectEnabled) {
-        const aboutMeElement = targetAboutMe.current;
-        const topOffset = aboutMeElement.getBoundingClientRect().top;
-        const isScrollingDown = topOffset <= 0;
-
-        if (isScrollingDown) {
-          aboutMeElement.scrollIntoView({ behavior: 'smooth' });
-        }
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [scrollEffectEnabled, targetAboutMe]);
-
-  const ScrollDownClick = () => {
-    setScrollEffectEnabled(true);
-    const aboutMeElement = targetAboutMe.current;
-    aboutMeElement.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
     <div className='App' ref={targetTop}>
       <div className='AppHeader' >
         Port Folio
       </div>
-      <div class="chevron"></div>
-      <div class="chevron"></div>
-      <div class="chevron"></div>
-      <span class="text" onClick={ScrollDownClick}>Scroll down</span>
       <div className='AppButtonSet'>
         <button onClick={TopClick} className='AppButtonStyle'>Top</button>
         <button onClick={AboutMeClick} className='AppButtonStyle'>AboutMe</button>
